35c0c7287e5f1db1117ef98039f73bb43b3df7d5
Sidiq Trihartanto <59751078+Trihartantosidiq@users.noreply.github.com>
2023-03-11 22:49:30 +0700

    Update koneksi.cs
    Username and Password

--- HSBC Letter/koneksi.cs
+++ HSBC Letter/koneksi.cs
@@ -15,18 +15,9 @@ namespace HSBC_Letter
         SqlConnection con;
         public SqlCommand cmd;
         SqlDataAdapter adapter;
-        //static string constr = Properties.Settings.Default.KonekSql.ToString();
+        static string constr = Properties.Settings.Default.KonekSql.ToString();
 
-        //Server=192.168.10.13; Initial Catalog=SINARMAS; User ID=Ivan.Witono; Password=Ivan1234!
-        //static string constr = Properties.Settings.Default.KonekLocal.ToString();
-        //static string constr = "Server=; Initial Catalog=Manulife_PAYDI; User ID=Ivan.Witono; Password=Ivan1234!";
-        //static SqlTransaction tran;
-        //string vNoman = "";
-        //static string constr = @"Data Source=LAPTOP-S496JAR6\DBSIDIQ; Initial Catalog=Manulife_PAYDI; Integrated Security=True"; //User ID=Ivan.Witono;Password=Ivan1234!";
-        //static string constr = "Data Source=192.168.10.13; Initial Catalog=AllianzAHCS; User ID=Ivan.Witono; Password=Ivan1234!";
-
-        //static string constr = @"Data Source=LAPTOP-S496JAR6\DBSIDIQ;Initial Catalog = Manulife_DAYCOMMS; Integrated Security = True";
-        static string constr = @"Data Source= LAPTOP-S496JAR6\DBSIDIQ;Initial Catalog = HSBC_Letter; Integrated Security = True";
+        
        
 
         // providerName="System.Data.SqlClient";
